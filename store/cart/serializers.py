371d13d4eb900b5071691f0f309c2c139f0ebb9c
perevozshikov <d0llar0v@yandex.ru>
2025-07-01 09:51:50 +0300

    .

--- store/cart/serializers.py
+++ store/cart/serializers.py
@@ -4,15 +4,32 @@ from catalog.models import Product
 from catalog.serializers import ProductSerializer
 
 
+# class CartItemSerializer(serializers.ModelSerializer):
+#     product = ProductSerializer(read_only=True)
+#     product_id = serializers.PrimaryKeyRelatedField(
+#         queryset=Product.objects.all(), source='product', write_only=True
+#     )
+#
+#     class Meta:
+#         model = CartItem
+#         fields = ['id', 'product', 'product_id', 'quantity', 'total_price']
+
 class CartItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
-    product_id = serializers.PrimaryKeyRelatedField(
-        queryset=Product.objects.all(), source='product', write_only=True
-    )
+    product_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'product_id', 'quantity', 'total_price']
+        fields = ['id', 'product', 'product_id', 'quantity']
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Product not found")
+        return value
+
+    def create(self, validated_data):
+        raise NotImplementedError("Use CartService instead")
+
 
 
 class CartSerializer(serializers.ModelSerializer):
