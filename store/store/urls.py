afb5783e76c23453e0eb1cd9a7c09ad96dfe3884
perevozshikov <d0llar0v@yandex.ru>
2025-07-02 12:50:28 +0300

    .

--- store/store/urls.py
+++ store/store/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('', include('catalog.urls')),
+    path('', include('catalog.urls'))
 ]
