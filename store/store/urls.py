380e58c06d59a79e102f116a21a6980649d9a861
perevozshikov <d0llar0v@yandex.ru>
2025-07-03 12:26:39 +0300

    .

--- store/store/urls.py
+++ store/store/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     path('api/categories/', CategoryViewSet.as_view(), name='api_categories_list'),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
